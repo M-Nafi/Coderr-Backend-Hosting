d50ac4b6a4f4a4c879c15852accda792c0dcdce1
Nafi Müftüoglu <140119132+M-Nafi@users.noreply.github.com>
2025-10-04 19:22:19 +0200

    Add dummy view for /api/

--- coderr/urls.py
+++ coderr/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
 
 urlpatterns = [
+    path('api/', lambda request: JsonResponse({'status': 'Coderr API läuft'})),
     path('admin/', admin.site.urls),
     path('api/', include('user_auth.api.urls')),
     path('api/', include('base_info.api.urls')),
